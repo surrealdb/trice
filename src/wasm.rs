85d1596ac7f1ce83cc9bf4280ae157ad42cb4677
Tobie Morgan Hitchcock <tobie@abcum.com>
2023-01-10 19:41:55 +0000

    Add ability to add and subtract `Duration` from `Instant` in WebAssembly

--- src/wasm.rs
+++ src/wasm.rs
@@ -8,28 +8,42 @@ pub struct Instant(Duration);
 impl Eq for Instant {}
 
 impl Instant {
-    #[inline]
-    pub fn now() -> Self {
-        Instant(duration_from_f64(now()))
-    }
-    #[inline]
-    pub fn elapsed(&self) -> Duration {
-        Self::now().duration_since(*self)
-    }
-    #[inline]
-    pub fn duration_since(&self, earlier: Instant) -> Duration {
-        self.0 - earlier.0
-    }
+	#[inline]
+	pub fn now() -> Self {
+		Instant(duration_from_f64(now()))
+	}
+	#[inline]
+	pub fn elapsed(&self) -> Duration {
+		Self::now().duration_since(*self)
+	}
+	#[inline]
+	pub fn duration_since(&self, earlier: Instant) -> Duration {
+		self.0 - earlier.0
+	}
 }
 
 fn duration_from_f64(millis: f64) -> Duration {
-    Duration::from_millis(millis.trunc() as u64)
-        + Duration::from_nanos((millis.fract() * 1.0e6) as u64)
+	Duration::from_millis(millis.trunc() as u64)
+		+ Duration::from_nanos((millis.fract() * 1.0e6) as u64)
 }
 
 fn now() -> f64 {
-    js_sys::Reflect::get(&js_sys::global(), &JsValue::from_str("performance"))
-        .expect("failed to get performance from global object")
-        .unchecked_into::<web_sys::Performance>()
-        .now()
+	js_sys::Reflect::get(&js_sys::global(), &JsValue::from_str("performance"))
+		.expect("failed to get performance from global object")
+		.unchecked_into::<web_sys::Performance>()
+		.now()
+}
+
+impl std::ops::Add<Duration> for Instant {
+	type Output = Instant;
+	fn add(self, other: Duration) -> Instant {
+		Instant(self.0.add(other))
+	}
+}
+
+impl std::ops::Sub<Duration> for Instant {
+	type Output = Instant;
+	fn sub(self, other: Duration) -> Instant {
+		Instant(self.0.sub(other))
+	}
 }
